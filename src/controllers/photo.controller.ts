ab983fb12662f16df408dd90aa0cb8af2fe56cff
hushino <frostshock@outlook.com>
2019-11-12 16:45:13 -0300

    image rezise

--- src/controllers/photo.controller.ts
+++ src/controllers/photo.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import Photo from '../models/Photo'
 import fs from 'fs-extra'
 import path from 'path'
+import sharp from 'sharp'
 
 export async function getPhotoById(req: Request, res: Response): Promise<Response> {
     const { id } = req.params
@@ -16,15 +17,21 @@ export async function getPhotos(req: Request, res: Response): Promise<Response> 
 }
 
 export async function createPhoto(req: Request, res: Response): Promise<Response> {
-    const { title, description } = req.body
+    const { title, description, image } = req.body
+
     const newPhoto = {
         title: title,
         description: description,
-        imagePath: req.file.path
+        imagePath: req.file.path + ".png"
     }
     const photo = new Photo(newPhoto)
-    //console.log(photo)
-    await photo.save();
+    await photo.save()
+    await sharp(req.file.path)
+        .jpeg({ quality: 50 })
+        .toFile(
+            path.resolve('./uploads/' + req.file.filename + ".png")
+        )
+    fs.unlinkSync(req.file.path)
     return res.json({
         message: 'Photo successfully saved',
         photo
