f12c7bd26b315f30756ddab77e299e478236ce1d
hushino <frostshock@outlook.com>
2019-11-20 09:55:55 -0300

    d

--- src/auth/hasAccess.ts
+++ src/auth/hasAccess.ts
@@ -2,8 +2,6 @@ import { Router, Request, Response, NextFunction } from 'express'
 
 export default function hasAccess(accessLevel: string) {
     return function (req: Request, res: Response, next: NextFunction) {
-        console.log(req.session.role+ ' sdas '+accessLevel)
-        // change search for haslocale and compare 1 to true
         if ( req.session.role &&  req.session.role === accessLevel) {
             console.log('entro al middleware')
             return next()
